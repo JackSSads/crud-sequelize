b90f4f9c61251760b251b8bf9fb859da9ea2d0c1
JackSSads <fotosjackson2018@hotmail.com>
2022-05-07 20:03:58 -0300

    Atualizando index

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-const conn = require('./db/conn');
-const User = require('./models/User');
-const Address = require('./models/Address');
+const conn = require("./db/conn");
+const Products = require("./models/Products");
 
 const app = express();
 
@@ -15,118 +14,61 @@ app.use(
 
 app.use(express.json());
 
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/users/create', (req, res) => {
-    res.render('addusers')
+app.get('/products', async (req, res) => {
+    const products = await Products.findAll({ raw: true });
+
+    res.render("products", { products });
 });
 
-app.post('/users/create', async (req, res) => {
-    const name = req.body.name;
-    const occupation = req.body.occupation;
-    let newsletter = req.body.newsletter;
+app.post("/addproducts", async (req, res) => {
+    const nameProduct = req.body.nameProduct;
+    const valueProduct = req.body.valueProduct;
 
-    if (newsletter === 'on') {
-        newsletter = true;
-    } else {
-        newsletter = false;
-    }
+    await Products.create({ nameProduct, valueProduct });
 
-    await User.create({ name, occupation, newsletter })
+    res.redirect("/");
+});
 
-    res.redirect('/')
+app.get('/editproduct/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const data = await Products.findOne({ raw: true, where: { id: id } });
+
+    res.render('editproduct', { data });
 })
 
-app.get('/users/:id', async (req, res) => {
+app.post('/update/:id', async (req, res) => {
     const id = req.params.id;
+    const nameProduct = req.body.nameProduct;
+    const valueProduct = req.body.valueProduct;
 
-    const users = await User.findOne({ raw: true, where: { id: id } });
+    const data = {
+        nameProduct,
+        valueProduct,
+    };
 
-    res.render('userview', { users });
+    await Products.update(data, { where: { id: id } });
+
+    res.redirect('/products');
 })
 
-app.post('/users/delete/:id', async (req, res) => {
+app.post('/delete/:id', async (req, res) => {
     const id = req.params.id;
 
-    await User.destroy({ where: { id: id } });
+    await Products.destroy({ where: { id: id }});
 
-    res.redirect('/');
+    res.redirect('/products');
 });
 
-app.get('/users/edit/:id', async (req, res) => {
-    const id = req.params.id;
-
-    const user = await User.findOne({ include: Address, where: { id: id } });
-
-    res.render('useredit', { user: user.get({ plain: true }) });
+app.get("/", (req, res) => {
+    res.render("home");
 });
 
-app.post('/users/update', async (req, res) => {
-    const id = req.body.id;
-    const name = req.body.name;
-    const occupation = req.body.occupation;
-    let newsletter = req.body.occupation;
-
-    if (newsletter === 'on') {
-        newsletter = true;
-    } else {
-        newsletter = false;
-    };
-
-    const userData = {
-        id,
-        name,
-        occupation,
-        newsletter,
-    };
-    try {
-        await User.update(userData, { where: { id: id } });
-    } catch (error) {
-        console.log(error);
-    };
-
-    res.redirect('/');
-});
-
-app.post("/addres/create/", async (req, res) => {
-    const UserId = req.body.UserId;
-    const street = req.body.street;
-    const number = req.body.number;
-    const city = req.body.city;
-
-    const address = {
-        UserId,
-        street,
-        number,
-        city,
-    };
-
-    await Address.create(address);
-
-    res.redirect(`/users/edit/${UserId}`);
-});
-
-app.post("/address/delete", async (req, res) => {
-    const UserId = req.body.UserId;
-    const id = req.body.id;
-
-    await Address.destroy({where: { id: id }});
-
-    res.redirect(`/users/edit/${UserId}`);
-});
-
-app.get('/', async (req, res) => {
-
-    const users = await User.findAll({ raw: true });
-
-    res.render('home', { users });
-});
-
-conn.sync()
-    //sync({force: true}) // Método {force: true} serve para apagar todos os dados da tabela
-    .then(() => {
-        app.listen(3000);
-    }).catch((err) => console.log(err));
+conn.sync().then(() => {
+    app.listen(3000);
+}).catch((err) => { console.log(err) });
